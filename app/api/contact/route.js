79ce0453504eec671c636e0dddf828e9ba0c8a2f
Cazim Meskovic <meskovic0007@gmail.com>
2025-05-09 09:04:32 +0200

    napravio kontakt radi

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -38,9 +38,10 @@ Message: ${message}
   }
 }
  */
+
+/* 
 import { Resend } from 'resend';
 
-/* kod za provjeru robota start */
 
 export async function POST(req) {
   const body = await req.json();
@@ -92,4 +93,37 @@ ${message}
   }
 }
 
-/* kod za provjeru robota end */
+
+ */
+
+import { Resend } from 'resend';
+
+export async function POST(req) {
+  const body = await req.json();
+  const { name, email, message, company, website } = body;
+
+
+  // Nastavi sa slanjem e-maila
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  try {
+    const data = await resend.emails.send({
+      from: 'onboarding@resend.dev', // zamijeni sa vlastitom ako budeš verifikovao domen
+      to: ['cazemeskovic@gmail.com'], // Promijeni sa stvarnim emailom
+      subject: `New contact form submission from ${name}`,
+      reply_to: email,
+      text: `
+Name: ${name}
+Email: ${email}
+Company: ${company || 'N/A'}
+Message:
+${message}
+      `,
+    });
+
+    return new Response('Message sent successfully', { status: 200 });
+  } catch (error) {
+    console.error('Resend Error:', error);
+    return new Response('Failed to send email', { status: 500 });
+  }
+}
