4d8f91d7d79543a7a1ea9665cabe2608a5fd9e18
Cazim Meskovic <meskovic0007@gmail.com>
2025-05-13 19:33:54 +0200

    radi  sesija i token i autorise ono 13.5 20h

--- app/dashboard/dashboardClient.jsx
+++ app/dashboard/dashboardClient.jsx
@@ -4,7 +4,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { supabase } from '@/utils/supabase/client';
+import { supabase } from '../utils/supabase/client';
 import Image from 'next/image';
 
 export default function DashboardClient() {
